ffbc0ee68ba6d6b3c99ca501306cbdcecb51b2ed
Menna Tawfiq <99543714+mennatawfiq@users.noreply.github.com>
2023-12-29 21:09:14 +0200

    Update templates.sql

---
+++ database/data/direct insertions/templates.sql
@@ -0,0 +1,15 @@
+INSERT INTO Templates(template_name, template_before, template_after, purchase_price, selling_price, in_stock)
+VALUES
+('Phone Cover', 'phoneCoverBefore.png', 'phoneCoverAfter.png', 5, 10, 20),
+('Mug', 'mugBefore.png', 'mugAfter.png', 5, 10, 20),
+('Coaster', 'coasterBefore.png', 'coasterAfter.png', 5, 10, 20),
+('Notebook', 'notebookBefore.png', 'notebookAfter.png', 8, 15, 20),
+('iPad Case', 'ipadCaseBefore.png', 'ipadCaseAfter.png', 8, 15, 20),
+('Laptop Sleeve', 'laptopSleeveBefore.png', 'laptopSleeveAfter.png', 10, 20, 20),
+('Sticker', 'stickerBefore.png', 'stickerAfter.png', 1, 2, 20),
+('Laptop Skin', 'laptopSkinBefore.png', 'laptopSkinAfter.png', 8, 15, 20),
+('T-Shirt', 'tshirtBefore.png', 'tshirtAfter.png', 10, 25, 20),
+('Tote Bag', 'toteBefore.png', 'toteAfter.png', 15, 30, 20),
+('Poster', 'posterBefore.png', 'posterAfter.png', 3, 5, 20),
+('Frame', 'frameBefore.png', 'frameAfter.png', 5, 10, 20),
+('A4', 'a4Before.png', 'a4After.png', 1, 2, 20);
