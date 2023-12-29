f5b5a25029737bec57e3945f1aff2b28b4e7aaa5
Menna Tawfiq <99543714+mennatawfiq@users.noreply.github.com>
2023-12-29 21:05:54 +0200

    Update users.sql

---
+++ database/data/direct insertions/users.sql
@@ -0,0 +1,26 @@
+INSERT INTO Users(user_name, email, account_type, gender, profile_photo, cover_photo, bio, password, joining_date, birth_date)
+VALUES
+('koronba', 'koronba@gmail.com', 'artist', 'female', 'profile2.jpg', 'cover2.jpg', "I'm Koronba", '$2b$10$RuwdwPShfWGWiJ2AS5sUg.ymf19khgizPsICg3wH.z1zC4LApEORK', '2021-01-20', '2003-09-20'), 
+('abo_seraj', 'abo_seraj@gmail.com', 'artist', 'female', 'profile6.jpg', 'cover6.jpg', "I'm abo seraj", '$2b$10$3V3AQDzor.7qHPRK2mbCae13cRvdIJhWOo/M0l10zbI6OdTngvvRS', '2021-02-20', '2004-01-01'),
+('fawzia', 'fawzia@gmail.com', 'artist', 'female', 'profile3.jpg', 'cover3.jpg', "I'm Fawzia", '$2b$10$Qq1wYRsL8KmaRgwiy5oS7OXEH18r3Xz3jSilamuvxG4Mdy2wyUC/e', '2021-03-20', '2003-04-20'),
+('shawkya', 'shawkya@gmail.com', 'artist', 'female', 'profile4.jpg', 'cover4.jpg', "I'm Shawkya", '$2b$10$IGnQKvvAETie1hwAA3vam.eVDbIgYLQksS/6SWWixeUqtcYTGyGVC', '2021-04-20', '2003-03-20'),
+('teko', 'teko@gmail.com', 'artist', 'female', 'profile1.jpg', 'cover1.jpg', "I'm Teko", '$2b$10$TteJc0BUw6uOlpYq9NKxbuPQTlzOOXlBJw3XoqI4GB4O3yk3UYmVK', '2021-05-20', '2002-08-20'),
+('sando', 'sando@gmail.com', 'artist', 'female', 'profile5.jpg', 'cover5.jpg', "I'm Sando", '$2b$10$as9aEyRQYDTfhtsBYMtJrugjlq5QooxwcRt8BZupSGw9P99fQzELC', '2021-06-20', '2003-11-10');
+INSERT INTO Users(user_name, email, account_type, gender, profile_photo, bio, password, joining_date, birth_date)
+VALUES
+('shokrya', 'shokrya@gmail.com', 'artist', 'female', 'artist_default.jpg', "I'm Shokrya", '$2b$10$3b084Hr3oFwleWbqBXSL/u28XpQY7toqVOl/wnvrKpM6hOCznJFFS', '2021-05-30', NULL),
+('nefisa', 'nefisa@gmail.com', 'artist', 'female', 'artist_default.jpg', "I'm Nefisa Nafosa", '$2b$10$iYG01FPwA8QjJSEWPnQM0uo30CpIC6RC.oXKJu4IjA6pxwhFT9ahe', '2021-05-30', NULL),
+('bora3y', 'bora3y@gmail.com', 'artist', 'male', 'artist_default.jpg', "I'm Bora3y", '$2b$10$DS8vPgVn33TJAgTyV1zPsOXyspB3JxxUzwWOcZIQ94Zvzwjkq/hKW', '2021-05-30', '2003-05-30'),
+('samara', 'smara@gmail.com', 'artist', 'female', 'artist_default.jpg', "I'm Samara Samora", '$2b$10$NgukMa3VOCEuPGLRFCMubeEAP8Z30gz2XWGWxxrAITqipRmZRB73S', '2021-05-30', '2000-12-03');
+INSERT INTO Users(user_name, email, account_type, gender, password, joining_date)
+VALUES
+('user1', 'user1@gmail.com', 'customer', 'male', '$2b$10$Ip84lkewqu28blu8Tknzfen5XskweQeoNaM6iWGY2kAuek1IA4HGa', '2021-12-11'),
+('user2', 'user2@gmail.com', 'customer', 'male', '$2b$10$Ip84lkewqu28blu8Tknzfen5XskweQeoNaM6iWGY2kAuek1IA4HGa', '2021-12-11'),
+('user3', 'user3@gmail.com', 'customer', 'female', '$2b$10$Ip84lkewqu28blu8Tknzfen5XskweQeoNaM6iWGY2kAuek1IA4HGa', '2021-12-11'),
+('user4', 'user4@gmail.com', 'customer', 'female', '$2b$10$Ip84lkewqu28blu8Tknzfen5XskweQeoNaM6iWGY2kAuek1IA4HGa', '2021-12-11'),
+('user5', 'user5@gmail.com', 'customer', 'male', '$2b$10$Ip84lkewqu28blu8Tknzfen5XskweQeoNaM6iWGY2kAuek1IA4HGa', '2021-12-11'),
+('user6', 'user6@gmail.com', 'customer', 'female', '$2b$10$Ip84lkewqu28blu8Tknzfen5XskweQeoNaM6iWGY2kAuek1IA4HGa', '2021-12-11'),
+('user7', 'user7@gmail.com', 'customer', 'female', '$2b$10$Ip84lkewqu28blu8Tknzfen5XskweQeoNaM6iWGY2kAuek1IA4HGa', '2021-12-11'),
+('user8', 'user8@gmail.com', 'customer', 'male', '$2b$10$Ip84lkewqu28blu8Tknzfen5XskweQeoNaM6iWGY2kAuek1IA4HGa', '2021-11-12'),
+('user9', 'user9@gmail.com', 'customer', 'male', '$2b$10$Ip84lkewqu28blu8Tknzfen5XskweQeoNaM6iWGY2kAuek1IA4HGa', '2021-12-11'),
+('user10', 'user10@gmail.com', 'customer', 'female', '$2b$10$Ip84lkewqu28blu8Tknzfen5XskweQeoNaM6iWGY2kAuek1IA4HGa', '2021-12-11');
